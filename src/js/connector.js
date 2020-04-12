daef4be968302d6ac99ccc701f0d41d19efe69f4
artur-h <artur.hlushko@gmail.com>
2020-04-12 23:12:30 +0300

    Moved render function from connector to modal js file

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,90 +1,12 @@
 const WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
 const BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
 
-function createUserList(data) {
-  const tmpArray = [];
-  const updatedUserList = [];
-  data.forEach( card => {
-    updatedUserList.push(...(card.members.filter( member => addUser(member, tmpArray))));
-  });
-
-  return updatedUserList;
-}
-
-function addUser(member, tmpArray) {
-  if (tmpArray.indexOf(member.id) === -1) {
-    tmpArray.push(member.id);
-    return true
-  }
-  return false;
-}
-
-function generateList(info) {
-  const userList = createUserList(info);
-  const allProjectsDoneByUser = {};
-  userList.forEach(user => {
-    allProjectsDoneByUser[user.id] = [];
-
-    info.forEach(card => card.members.forEach( member => {
-      if (member.id === user.id) {
-        allProjectsDoneByUser[user.id].push(card);
-      }
-    }))
-  })
-
-  return {
-    data: info,
-    userList: userList,
-    allProjectsDoneByUser: allProjectsDoneByUser
-  }
-}
-
-function renderUserList(projects, users) {
-  for (let key in projects) {
-    document.getElementById('list').innerHTML += formatUserList(key, users, projects)
-    console.log(document.getElementById('list'));
-  }
-}
-
-function formatUserList(userId, users, projects) {
-  const user = users.find(currentUser => {
-    if (currentUser.id === userId) return true;
-  })
-
-  return `<div class="employee">
-    <h3 class="employee__name">${user.fullName}</h3>
-    <div class="employee__username">${user.username}</div>
-    <img src="${user.avatar}" alt="Employee's avatar" class="employee__avatar">
-    ${renderProjectList(user.id, projects)}
-    </div>
-  `
-}
-
-function renderProjectList(id, projects) {
-  return `
-    <ol class="project">
-      ${projects[id].map(card => formatProjectList(card)).join('')}
-    </ol>
-  `
-}
-
-function formatProjectList(project) {
-  return `
-    <li>
-      <span class="project__status">List name</span>
-      <a href="${project.url}" class="project__card-link">${project.name}</a>
-    </li>
-  `
-}
-
-
 function onBtnClick(t, opts) {
   return t.modal({
     url: 'https://wedcat-trello-power-up.netlify.com/modal.html',
     fullscreen: true
   })
 }
-
 
 window.TrelloPowerUp.initialize({
   'board-buttons': function (t, opts) {
@@ -99,17 +21,3 @@ window.TrelloPowerUp.initialize({
     }
   }
 });
-
-// function onBtnClick(t, opts) {
-//   return t.cards('all')
-//     .then(function (cards) {
-//       return generateList(cards);
-//     }).then(function (info) {
-//       return renderUserList(info.allProjectsDoneByUser, info.userList)
-//     }).then(function () {
-//       return t.modal({
-//         url: 'https://wedcat-trello-power-up.netlify.com/',
-//         fullscreen: true,
-//       })
-//     });
-// }
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,86 @@
 let t = window.TrelloPowerUp.iframe();
 
-console.log('modal.js')
+t.render(function () {
+  return t.cards('all')
+    .then(function (cards) {
+      return generateList(cards);
+    }).then(function (info) {
+      return renderUserList(info.allProjectsDoneByUser, info.userList);
+    })
+})
+
+function createUserList(data) {
+  const tmpArray = [];
+  const updatedUserList = [];
+  data.forEach( card => {
+    updatedUserList.push(...(card.members.filter( member => addUser(member, tmpArray))));
+  });
+
+  return updatedUserList;
+}
+
+function addUser(member, tmpArray) {
+  if (tmpArray.indexOf(member.id) === -1) {
+    tmpArray.push(member.id);
+    return true
+  }
+  return false;
+}
+
+function generateList(info) {
+  const userList = createUserList(info);
+  const allProjectsDoneByUser = {};
+  userList.forEach(user => {
+    allProjectsDoneByUser[user.id] = [];
+
+    info.forEach(card => card.members.forEach( member => {
+      if (member.id === user.id) {
+        allProjectsDoneByUser[user.id].push(card);
+      }
+    }))
+  })
+
+  return {
+    data: info,
+    userList: userList,
+    allProjectsDoneByUser: allProjectsDoneByUser
+  }
+}
+
+function renderUserList(projects, users) {
+  for (let key in projects) {
+    document.getElementById('list').innerHTML += formatUserList(key, users, projects)
+    console.log(document.getElementById('list'));
+  }
+}
+
+function formatUserList(userId, users, projects) {
+  const user = users.find(currentUser => {
+    if (currentUser.id === userId) return true;
+  })
+
+  return `<div class="employee">
+    <h3 class="employee__name">${user.fullName}</h3>
+    <div class="employee__username">${user.username}</div>
+    <img src="${user.avatar}" alt="Employee's avatar" class="employee__avatar">
+    ${renderProjectList(user.id, projects)}
+    </div>
+  `
+}
+
+function renderProjectList(id, projects) {
+  return `
+    <ol class="project">
+      ${projects[id].map(card => formatProjectList(card)).join('')}
+    </ol>
+  `
+}
+
+function formatProjectList(project) {
+  return `
+    <li>
+      <span class="project__status">List name</span>
+      <a href="${project.url}" class="project__card-link">${project.name}</a>
+    </li>
+  `
+}
